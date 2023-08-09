18b8844039b530a849a87ed762fc71aee2cefff5
orch20 <94862153+orch20@users.noreply.github.com>
2023-08-09 11:45:33 +0300

    move logic to computed and watch

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,25 @@ export default {
     return {
       ticker: null,
       tickers:[],
-      sel: null,
+      selectedTicker: null,
       graph: [],
       page: 1,
       filter: '',
-      hasNextPage: true,
+      // hasNextPage: true,
     }
     
   },
 
   created() { 
+    const windowData = Object.fromEntries(new URL(window.location).searchParams.entries());
+    console.log(windowData);
+    if (windowData.page) {
+      this.page = windowData.page;
+    }
+    if (windowData.filter) { 
+      this.filter = windowData.filter;
+    }
+    
     const tickersData = localStorage.getItem("tickers-data")
 
     if (tickersData) {
@@ -22,62 +31,112 @@ export default {
       this.tickers.forEach(ticker => this.subscribeToUpdates(ticker.name))
     }
   },
-      methods: {
-        add(){
-          const currentTicker = { name: this.ticker, price: "-", };
-          this.tickers.push(currentTicker);
 
-          localStorage.setItem("tickers-data", JSON.stringify(this.tickers));
-          this.subscribeToUpdates(currentTicker.name)
-          this.ticker = "";
-          this.filter = "";
-        },
+  computed: {
+    startIndex() {
+  return (this.page - 1) * 6
+    },
 
-        handleDelete(tickerToRemove) {
-          this.tickers = this.tickers.filter(t => t !== tickerToRemove)
-        },
+    endIndex() {
+    return this.page * 6
+    },
 
-        filterTickers() {
-          const start = (this.page - 1) * 6
-          const end = this.page * 6 
 
-          const filteredTickers = this.tickers.filter(t => t.name?.includes(this.filter))
+    filteredTickers() {
+      return this.tickers.filter(t => t.name?.includes(this.filter))
+    },
 
-          this.hasNextPage = filteredTickers.length > end
+    paginatedTickers() {
+      return this.filteredTickers.slice(this.startIndex, this.endIndex)
+     },
 
-          return filteredTickers.slice(start, end)
-},
+    hasNextPage() {
+      return this.filteredTickers.length > this.endIndex
+    },
 
-        subscribeToUpdates(tickerName) {
-  setInterval(async () => {
-        const f = await fetch(`https://min-api.cryptocompare.com/data/price?fsym=${tickerName}&tsyms=USD&api_key={b1ebb3070a82f30c59ba952fcc4c27808d894ee17fd62edfaca2eb9612bdb64e}`)
-        const data = await f.json();
-        this.tickers.find(ticker => ticker.name === tickerName).price = data?.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
-        // currentTicker.price = data.USD
-        if (this.sel?.name === tickerName) {
-          this.graph.push(data.USD);
-
-        }
-      }, 3000);
-},
-
-    normalizeGraph() {
+    normalizedGraph() {
       const maxValue = Math.max(...this.graph)
       const minValue = Math.min(...this.graph)
+
+      if (maxValue === minValue) {
+        return this.graph.map(()=> 50)
+      }
       return this.graph.map(price => 5 +
         (price - minValue) * 95 / (maxValue - minValue)
       )
-    }, 
+    },
+    pageStateOptions() {
+      return {
+        filter: this.filter,
+        pageState: this.pageState
+      }
+    }
+
+  },
+
+  methods: {
+    add(){
+      const currentTicker = { name: this.ticker, price: "-", };
+      this.tickers = [...this.tickers, currentTicker];
+
+      
+      this.subscribeToUpdates(currentTicker.name)
+      this.ticker = "";
+      this.filter = "";
+    },
+
+    handleDelete(tickerToRemove) {
+      this.tickers = this.tickers.filter(t => t !== tickerToRemove)
+      if (this.selectedTicker === tickerToRemove) {
+        this.selectedTicker = null
+      }
+    },
+
+    
+    subscribeToUpdates(tickerName) {
+      setInterval(async () => {
+      const f = await fetch(`https://min-api.cryptocompare.com/data/price?fsym=${tickerName}&tsyms=USD&api_key={b1ebb3070a82f30c59ba952fcc4c27808d894ee17fd62edfaca2eb9612bdb64e}`)
+      const data = await f.json();
+      this.tickers.find(ticker => ticker.name === tickerName).price = data?.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
+      // currentTicker.price = data.USD
+      if (this.selectedTicker?.name === tickerName) {
+      this.graph.push(data.USD);
+
+    }
+  }, 3000);
+  },
 
     select(ticker) {
-      this.sel = ticker;
-      this.graph = []; 
-        },
+      this.selectedTicker = ticker;
+    },
 
-    watch: {
-      filter() {
+
+  },
+
+  watch: {
+    selectedTicker() {
+      this.graph = [];
+    },
+
+    ticker() {
+      localStorage.setItem("tickers-data", JSON.stringify(this.tickers));
+    },
+
+    paginatedTickers() {
+      if (this.paginatedTickers.length === 0 && this.page > 1) {
         this.page = 1
       }
+    },
+
+    filter() {
+      this.page = 1;      
+    },
+    pageStateOptions(value) {
+        window.history.pushState(
+        null,
+        document.title,
+        `${window.location.pathname}?filter=${value.filter}&page=${value.page}`
+      )
     }
   }
 }
@@ -170,11 +229,11 @@ export default {
         <hr class="w-full border-t border-gray-600 my-4" />
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
           <div 
-            v-for="t in filterTickers()"
+            v-for="t in paginatedTickers"
             v-bind:key="t.name"
             @click="select(t)" 
             :class="{
-              'border-4' : sel === t
+              'border-4' : selectedTicker === t
             }"
             class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
           >
@@ -209,13 +268,13 @@ export default {
         </dl>
         <hr class="w-full border-t border-gray-600 my-4" />
         </template>
-      <section v-if="this.sel" class="relative">
+      <section v-if="this.selectedTicker" class="relative">
         <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-          {{ sel.name }} - USD
+          {{ selectedTicker.name }} - USD
         </h3>
         <div class="flex items-end border-gray-600 border-b border-l h-64">
           <div
-          v-for="(bar, idx) in normalizeGraph()"
+          v-for="(bar, idx) in normalizedGraph"
           :key="idx"
           :style='{ height: `${bar}%` }'
             class="bg-purple-800 border w-10"
@@ -223,7 +282,7 @@ export default {
           
         </div>
         <button
-        @click = "sel= null"
+        @click = "selectedTicker= null"
           type="button"
           class="absolute top-0 right-0"
         >
